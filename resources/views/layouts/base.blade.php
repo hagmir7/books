0836083c60072d0fd248bdaa571d9b7f2750b8fc
Hassan Agmir <hagmir6@gmail.com>
2024-06-17 12:17:15 +0100

    My update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -112,7 +112,7 @@
 
 
 
-    <script>
+    {{-- <script>
         $('.home-book-carousel').owlCarousel({
             center: true,
             items: 3,
@@ -145,7 +145,7 @@
     @yield('footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+    </script> --}}
 </body>
 
 </html>
