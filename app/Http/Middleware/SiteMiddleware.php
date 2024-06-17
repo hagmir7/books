92f9df6229cf67eb9de91c8a06db4bfddd0bac7d
Hassan Agmir <hagmir6@gmail.com>
2024-06-17 11:32:16 +0100

    New udpate

--- app/Http/Middleware/SiteMiddleware.php
+++ app/Http/Middleware/SiteMiddleware.php
@@ -7,6 +7,8 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Str;
 
 class SiteMiddleware
 {
@@ -19,6 +21,20 @@ class SiteMiddleware
         $currentUrl = request()->getSchemeAndHttpHost();
 
         config(['app.url' => $currentUrl]);
+
+        $cookieName = 'o2s-chl';
+
+        if (!Cookie::has($cookieName)) {
+            $cookieValue = Str::random(32); // Generate a random 32-character string
+            $minutes = 60; // Cookie duration in minutes
+
+            // Set the cookie
+            Cookie::queue($cookieName, $cookieValue, $minutes);
+
+            session()->flash('message', 'Cookie has been set.');
+        } else {
+            session()->flash('message', 'Cookie already exists.');
+        }
 
 
         View::share('site', $site);
