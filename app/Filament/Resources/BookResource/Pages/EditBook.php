6af9c9207ba2f8c48043ba9b8cea2b45ae597d7e
Hassan Agmir <hagmir6@gmail.com>
2024-07-20 11:58:10 +0100

    add navigations

--- app/Filament/Resources/BookResource/Pages/EditBook.php
+++ app/Filament/Resources/BookResource/Pages/EditBook.php
@@ -2,19 +2,28 @@
 
 namespace App\Filament\Resources\BookResource\Pages;
 
+
 use App\Filament\Resources\BookResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use JoseEspinal\RecordNavigation\Traits\HasRecordNavigation;
+
 
 class EditBook extends EditRecord
 {
+
+    use HasRecordNavigation;
+
+
     protected static string $resource = BookResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [
+
+        $existingActions = [
             Actions\DeleteAction::make(),
         ];
+        return array_merge($existingActions, $this->getNavigationActions());
     }
 
 }
