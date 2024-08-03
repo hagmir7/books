84fc896a839c58b86a48f8c540e912421dd33aaf
Hassan Agmir <hagmir6@gmail.com>
2024-08-03 10:38:05 +0100

    New updates

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -6,7 +6,7 @@ use App\Models\Author;
 use App\Models\Book;
 use App\Models\BookCategory;
 use App\Models\Post;
-use Illuminate\Http\Request;
+use App\Models\Site;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Tags\Url;
 
@@ -15,14 +15,20 @@ class SitemapController extends Controller
     public function index()
     {
 
+        $sitemap_name = explode(".", str_replace('www.', '', request()->host()))[0];
+
+        $site = Site::where('domain', $sitemap_name)->firstOrFail();
+
+        $site_options = json_decode($site->site_options, true);
+
+
+
         $sitemap = Sitemap::create()
             ->add(Url::create('/'))
-            ->add(Url::create('/books'))
-            ->add(Url::create('/books'))
             ->add(Url::create('/contact-us'))
             ->add(Url::create('/blog'))
             ->add(Url::create('/books'))
-            ->add(Url::create('/authors'));;
+            ->add(Url::create('/authors'));
 
 
         Book::all()->each(function (Book $book) use ($sitemap) {
@@ -37,11 +43,33 @@ class SitemapController extends Controller
             $sitemap->add(Url::create("/category/{$cateogry->slug}"));
         });
 
-        Post::all()->each(function (Post $post) use ($sitemap) {
+
+
+        // if ($site_options['home_page'] == "books") {
+
+        //     $sitemap = Sitemap::create()
+        //         ->add(Url::create('/'))
+        //         ->add(Url::create('/books'))
+        //         ->add(Url::create('/authors'));
+
+        //     Book::all()->each(function (Book $book) use ($sitemap) {
+        //         $sitemap->add(Url::create("/books/{$book->slug}"));
+        //     });
+
+        //     Author::all()->each(function (Author $author) use ($sitemap) {
+        //         $sitemap->add(Url::create("/authors/{$author->slug}/books"));
+        //     });
+
+        //     BookCategory::all()->each(function (BookCategory $cateogry) use ($sitemap) {
+        //         $sitemap->add(Url::create("/category/{$cateogry->slug}"));
+        //     });
+        // }
+
+
+
+        Post::where('site_id', $site->id)->each(function (Post $post) use ($sitemap) {
             $sitemap->add(Url::create("/blog/{$post->slug}"));
         });
-
-        $sitemap_name = explode(".", str_replace('www.', '', request()->host()))[0];
         $sitemap->writeTofile(public_path($sitemap_name.'.xml'));
 
         return "Sitemap generated successfully!";
