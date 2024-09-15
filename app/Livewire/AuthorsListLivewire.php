a598f9a08da75c1aacaf4eab1354257fbb4f05b0
Hassan Agmir <hagmir6@gmail.com>
2024-09-15 15:13:17 +0100

    New u

--- app/Livewire/AuthorsListLivewire.php
+++ app/Livewire/AuthorsListLivewire.php
@@ -14,16 +14,15 @@ class AuthorsListLivewire extends Component
 
     public function render()
     {
-        $authors =
-            Author::select('authors.*')
-            ->join('books', 'books.author_id', '=', 'authors.id')
-            ->withCount('books')
-            ->groupBy('authors.id')
-            ->orderByDesc('books_count')
-            ->limit(30)
-            ->get();
+        // $authors = Author::select('authors.*')
+        // ->join('books', 'books.author_id', '=', 'authors.id')
+        // ->withCount('books')
+        //     ->groupBy('authors.id')
+        //     ->orderByDesc('books_count')
+        //     ->limit(30)
+        //     ->get();
         return view('livewire.authors-list-livewire', [
-            'authors' =>  $authors
+            'authors' =>  Author::withCount('books')->orderBy('books_count', 'desc')->paginate(20)
         ]);
     }
 }
