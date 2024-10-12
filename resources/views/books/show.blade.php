2776561fe361f2e378dfe36a43aeb189d4c35cf0
Hassan Agmir <hagmir6@gmail.com>
2024-10-12 19:30:53 +0100

    For wecima

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -79,8 +79,8 @@
 
                     <!-- ads before download -->
                     <div class="download read py-3">
-                        {!! app("site")->ads !!}
-                        <a download href="{{ Storage::url($book->file) }}" class="w-100 btn btn-primary btn-rounded d-flex justify-content-center">
+                        {{-- {!! app("site")->ads !!} --}}
+                        <a download href="https://freedaz.com/book/{{ $book->slug }}" class="w-100 btn btn-primary btn-rounded d-flex justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                                 class="icon icon-tabler icons-tabler-outline icon-tabler-download">
@@ -92,7 +92,7 @@
                             <span class="mx-2">{{ __("Download") }}</span>
                         </a>
 
-                        <a href="{{ Storage::url($book->file) }}" class="w-100 mt-4 btn btn-warning btn-rounded d-flex justify-content-center">
+                        <a href="https://freedaz.com/book/{{ $book->slug }}" class="w-100 mt-4 btn btn-warning btn-rounded d-flex justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                                 class="icon icon-tabler icons-tabler-outline icon-tabler-book">
@@ -104,9 +104,8 @@
                                 <path d="M21 6l0 13" />
                             </svg>
                             <span class="mx-2">{{ __("Read") }}</span>
-
                         </a>
-                        {!! app("site")->ads !!}
+                        {{-- {!! app("site")->ads !!} --}}
                     </div>
                 </div>
             </div>
