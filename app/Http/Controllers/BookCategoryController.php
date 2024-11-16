e5971b3035282126d309a707266c42727b1998c5
Hassan Agmir <hagmir6@gmail.com>
2024-11-16 19:14:21 +0100

    Add long text

--- app/Http/Controllers/BookCategoryController.php
+++ app/Http/Controllers/BookCategoryController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class BookCategoryController extends Controller
 {
+
+
+    // public function
     public function show(BookCategory $category)
     {
         return view("categories.show", compact("category"));
