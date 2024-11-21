24d1abcbd0b2c50470632ce125d9822316f629bb
Hassan Agmir <hagmir6@gmail.com>
2024-11-21 19:50:08 +0100

    New updates

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -91,49 +91,35 @@ class PostResource extends Resource
                             ->live()
                             ->label('Markdown'),
 
-                // Forms\Components\RichEditor::make('body')
-                //     ->label(__('Content'))
-                //     ->required()
-                //     ->hidden(fn (Get $get): bool => $get('markdown'))
-                //     ->columnSpanFull(),
-
-
-                        Forms\Components\Textarea::make('body')
+                        Forms\Components\RichEditor::make('body')
                             ->label(__('Content'))
                             ->required()
-                            ->rows(8)
-                            // ->hidden(fn(Get $get): bool => $get('markdown'))
+                            ->hidden(fn (Get $get): bool => $get('markdown'))
                             ->columnSpanFull(),
 
 
-                        // Forms\Components\MarkdownEditor::make('body')
-                        //     ->label(__('Content'))
-                        //     ->required()
-                        //     ->hidden(fn (Get $get): bool => !$get('markdown'))
-                        //     ->columnSpanFull()
-                        //     ->toolbarButtons([
-                        //         'attachFiles',
-                        //         'blockquote',
-                        //         'bold',
-                        //         'bulletList',
-                        //         'codeBlock',
-                        //         'heading',
-                        //         'italic',
-                        //         'link',
-                        //         'orderedList',
-                        //         'redo',
-                        //         'strike',
-                        //         'table',
-                        //         'undo',
-                        //     ]),
 
-                        // Forms\Components\Textarea::make('body')
-                        //     ->label(__('Content'))
-                        //     ->required()
-                        //     ->rows(10)
-                        //     ->hidden(fn (Get $get): bool => !$get('markdown'))
-                        //     ->columnSpanFull(),
 
+                        Forms\Components\MarkdownEditor::make('body')
+                            ->label(__('Content'))
+                            ->required()
+                            ->hidden(fn (Get $get): bool => !$get('markdown'))
+                            ->columnSpanFull()
+                            ->toolbarButtons([
+                                'attachFiles',
+                                'blockquote',
+                                'bold',
+                                'bulletList',
+                                'codeBlock',
+                                'heading',
+                                'italic',
+                                'link',
+                                'orderedList',
+                                'redo',
+                                'strike',
+                                'table',
+                                'undo',
+                            ]),
 
                     ])->columns(2),
 
