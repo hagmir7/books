b5d9051bc016279becdc48bf72a700d4852677ea
Hassan Agmir <hagmir6@gmail.com>
2024-11-27 21:42:24 +0100

    Add multple languages

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(App\Http\Middleware\SiteMiddleware::class);
+        $middleware->append(App\Http\Middleware\SetLocale::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
 
