d9d1d86f24b7ca6e646a78c6b12bb53499363d37
Hassan Agmir <hagmir6@gmail.com>
2024-12-08 20:20:12 +0100

    Add author description generater

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,9 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Parsedown;
 
 class AuthorController extends Controller
 {
+
+
+    public function generate($author)
+    {
+        $response = Http::get('https://api.facepy.com/en/ai?author=' . $author->full_name);
+        if ($response->successful()) {
+            $parsedown = new Parsedown();
+            $description = $parsedown->text($response->json()['message']);
+
+            $author->description = $description;
+            $author->save();
+        } else {
+            abort($response->status(), 'Error fetching JSON file.');
+        }
+    }
+
+
     public function index()
     {
         return view("authors.index", [
@@ -16,6 +35,9 @@ class AuthorController extends Controller
     }
 
     public function show(Author $author){
+        if(!$author->description){
+            $this->generate($author);
+        }
         $books = $author->books()->paginate(18);
         $title = str_replace(":attr", $author->full_name, app('site')->site_options['author_title']);
         return view('authors.show', compact('author', 'books', 'title'));
