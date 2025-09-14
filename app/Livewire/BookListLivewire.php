37f8747b1f13290077e65b9c0440952bc5e309ff
Agmir Hassane <hagmir6@gmail.com>
2025-09-14 20:02:11 +0100

    Add page pagination

--- app/Livewire/BookListLivewire.php
+++ app/Livewire/BookListLivewire.php
@@ -9,7 +9,7 @@ use Livewire\Component;
 class BookListLivewire extends Component
 {
 
-    public $amount = 15;
+    public $amount = 20;
     public $total = 0;
 
     public BookCategory $category;
@@ -23,7 +23,7 @@ class BookListLivewire extends Component
 
     public function loadMore()
     {
-        $this->amount += 15;
+        $this->amount += 20;
     }
     public function render()
     {
