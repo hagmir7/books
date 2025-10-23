f134c19278beef1ea906ab5a804a48441efb7227
Agmir Hassane <hagmir6@gmail.com>
2025-10-23 22:41:09 +0100

    UPdate livewire

--- app/Livewire/BookListLivewire.php
+++ app/Livewire/BookListLivewire.php
@@ -9,7 +9,7 @@ use Livewire\Component;
 class BookListLivewire extends Component
 {
 
-    public $amount = 9;
+    public $amount = 27;
     public $total = 0;
 
     public BookCategory $category;
@@ -23,7 +23,7 @@ class BookListLivewire extends Component
 
     public function loadMore()
     {
-        $this->amount += 30;
+        $this->amount += 27;
     }
 
     public function render()
