6c33a5a9a3737db09ee305ba9d847693a4be4d25
Agmir Hassane <hagmir6@gmail.com>
2025-11-08 17:56:24 +0100

    Best new Updates

--- resources/views/books/redirect.blade.php
+++ resources/views/books/redirect.blade.php
@@ -1,5 +1,5 @@
 <script>
     setTimeout(() => {
-    window.location.href = "https://yakk.com/books/{{ $book->slug }}";
+    window.location.href = "https://yakk.shop/books/{{ $book->slug }}";
     }, 1000);
 </script>
